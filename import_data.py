0ce2e9b477cfd45d2b69c093def818c81a277d75
astertournoy <62717135+astertournoy@users.noreply.github.com>
2023-03-28 10:34:35 +0200

    making library to find preferred mode of choice per ID

--- import_data.py
+++ import_data.py
@@ -16,15 +16,31 @@ df = pd.read_csv(path, sep="\t")
 
 # sort out choices of transport mode per ID
 df.sort_index()
-all_choices = df.Choice
-all_ID = df.ID
-
-
-amount_Id = df.ID.value_counts(normalize=False, sort=True, ascending=False, bins=None, dropna=True)
-print(len(amount_Id))
 
 
 
 
+# We make a dictionary per ID, that says how often that ID chooses 0 (public transport),\
+# 1 (private mode) or 2 (soft mode)
+
+
+# Create an empty dictionary to store the results
+result_dict = {}
+
+# Loop through each unique index value
+for idx in df['ID'].unique():
+    # Subset the dataframe for the current index value
+    sub_df = df[df['ID'] == idx]
+
+    # Count the occurrences of 0, 1, and 2 in the 'Choice' column
+    count_0 = (sub_df['Choice'] == 0).sum()
+    count_1 = (sub_df['Choice'] == 1).sum()
+    count_2 = (sub_df['Choice'] == 2).sum()
+
+    # Add the counts to the dictionary
+    result_dict[idx] = {'count_0': count_0, 'count_1': count_1, 'count_2': count_2}
+
+
+# print(result_dict)
 
 
