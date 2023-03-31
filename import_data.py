7a8d2bfd2ca40cff04b2ba9fb207b6e7bac11871
astertournoy <62717135+astertournoy@users.noreply.github.com>
2023-03-31 15:53:00 +0200

    print 3D plot of which mode is used how often

--- import_data.py
+++ import_data.py
@@ -27,60 +27,51 @@ df.sort_index()
 
 # Create an empty dictionary to store the results
 result_dict = {}
-
 # Loop through each unique index value
 for idx in df['ID'].unique():
     # Subset the dataframe for the current index value
     sub_df = df[df['ID'] == idx]
-
     # Count the occurrences of 0, 1, and 2 in the 'Choice' column
     count_0 = (sub_df['Choice'] == 0).sum()
     count_1 = (sub_df['Choice'] == 1).sum()
     count_2 = (sub_df['Choice'] == 2).sum()
-
     # Add the counts to the dictionary
     result_dict[idx] = {'public_transport': count_0, 'private_mode': count_1, 'soft_mode': count_2}
 
+# print(len(result_dict))
 
 
-# # Define your dictionary with data
-# data = {
-#     'id1': {'public_transport': 10, 'private_mode': 20, 'soft_mode': 5},
-#     'id2': {'public_transport': 15, 'private_mode': 25, 'soft_mode': 10},
-#     'id3': {'public_transport': 20, 'private_mode': 30, 'soft_mode': 15},
-#     'id4': {'public_transport': 25, 'private_mode': 35, 'soft_mode': 20},
-#     'id5': {'public_transport': 30, 'private_mode': 40, 'soft_mode': 25}
-# }
-#
-# # Create an empty list to store the public_transport values
-# public_transport_values = []
-#
-# # Loop through each id in the dictionary and extract the public_transport value
-# for id, values in data.items():
-#     public_transport_values.append(values['public_transport'])
-#
-# # Print the list of public_transport values
-# print(public_transport_values)
+# Create an empty list to store the public_transport values
+public_transport_values = []
+private_mode_values = []
+soft_mode_values = []
+
+# add values of data to lists
+for id, values in result_dict.items():
+    public_transport_values.append(values['public_transport'])
+    private_mode_values.append(values['private_mode'])
+    soft_mode_values.append(values['soft_mode'])
 
 
-
-# Extract the data for each axis
-# x = result_dict('public_transport')
-# y = result_dict['private_mode']
-# z = result_dict['soft_mode']
+# plot data
+x = public_transport_values
+y = private_mode_values
+z = soft_mode_values
 
 # Create the 3D plot
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
-# ax.scatter(x, y, z)
-#
-# # Label the axes
-# ax.set_xlabel('Public Transport')
-# ax.set_ylabel('Private Mode')
-# ax.set_zlabel('Soft Mode')
-#
-# # Show the plot
-# plt.show()
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(x, y, z)
+
+# Label the axes
+ax.set_xlabel('Public Transport')
+ax.set_ylabel('Private Mode')
+ax.set_zlabel('Soft Mode')
+
+# Show the plot
+plt.show()
+
+
 
 
 
