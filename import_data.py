5b1479f07dd787ccd1819c965d6dc710f77cfeec
astertournoy <62717135+astertournoy@users.noreply.github.com>
2023-03-31 15:35:52 +0200

    working on getting the right data

--- import_data.py
+++ import_data.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
+from mpl_toolkits.mplot3d import Axes3D
 
 
 path = os.getcwd() + '\ModeChoiceOptima.txt'
@@ -38,9 +39,48 @@ for idx in df['ID'].unique():
     count_2 = (sub_df['Choice'] == 2).sum()
 
     # Add the counts to the dictionary
-    result_dict[idx] = {'count_0': count_0, 'count_1': count_1, 'count_2': count_2}
+    result_dict[idx] = {'public_transport': count_0, 'private_mode': count_1, 'soft_mode': count_2}
 
 
-# print(result_dict)
+
+# # Define your dictionary with data
+# data = {
+#     'id1': {'public_transport': 10, 'private_mode': 20, 'soft_mode': 5},
+#     'id2': {'public_transport': 15, 'private_mode': 25, 'soft_mode': 10},
+#     'id3': {'public_transport': 20, 'private_mode': 30, 'soft_mode': 15},
+#     'id4': {'public_transport': 25, 'private_mode': 35, 'soft_mode': 20},
+#     'id5': {'public_transport': 30, 'private_mode': 40, 'soft_mode': 25}
+# }
+#
+# # Create an empty list to store the public_transport values
+# public_transport_values = []
+#
+# # Loop through each id in the dictionary and extract the public_transport value
+# for id, values in data.items():
+#     public_transport_values.append(values['public_transport'])
+#
+# # Print the list of public_transport values
+# print(public_transport_values)
+
+
+
+# Extract the data for each axis
+# x = result_dict('public_transport')
+# y = result_dict['private_mode']
+# z = result_dict['soft_mode']
+
+# Create the 3D plot
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
+# ax.scatter(x, y, z)
+#
+# # Label the axes
+# ax.set_xlabel('Public Transport')
+# ax.set_ylabel('Private Mode')
+# ax.set_zlabel('Soft Mode')
+#
+# # Show the plot
+# plt.show()
+
 
 
