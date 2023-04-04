fc3992949b08939cfe56bceb4d2e3c1b8cd13db2
astertournoy <62717135+astertournoy@users.noreply.github.com>
2023-04-04 10:52:46 +0200

    finished adding commune type

--- Subquestion_1.py
+++ Subquestion_1.py
@@ -63,9 +63,61 @@ ax.set_zlabel('Soft Mode')
 plt.show()
 
 
+# Now we want to check how many people live in the different types of communes to have a rough idea.
+# Initialize a counter variable to 0
+count_tc_1 = 0
+count_tc_2 = 0
+count_tc_3 = 0
+count_tc_4 = 0
+count_tc_5 = 0
+count_tc_6 = 0
+count_tc_7 = 0
+count_tc_8 = 0
+count_tc_9 = 0
 
+# Loop through each key-value pair in the dictionary
+for idx, values in result_dict.items():
+    # Check if the value of 'TC' is equal to 1
+    if values['TC'] == 1:
+        # If so, increment the counter variable
+        count_tc_1 += 1
+    elif values['TC'] == 2:
+        # If so, increment the counter variable
+        count_tc_2 += 1
+    elif values['TC'] == 3:
+        # If so, increment the counter variable
+        count_tc_3 += 1
+    elif values['TC'] == 4:
+        # If so, increment the counter variable
+        count_tc_4 += 1
+    elif values['TC'] == 5:
+        # If so, increment the counter variable
+        count_tc_5 += 1
+    elif values['TC'] == 6:
+        # If so, increment the counter variable
+        count_tc_6 += 1
+    elif values['TC'] == 7:
+        # If so, increment the counter variable
+        count_tc_7 += 1
+    elif values['TC'] == 8:
+        # If so, increment the counter variable
+        count_tc_8 += 1
+    elif values['TC'] == 9:
+        # If so, increment the counter variable
+        count_tc_9 += 1
 
+print('centers', count_tc_1)
+print('suburban', count_tc_2)
+print('high-income', count_tc_3)
+print('periurban', count_tc_4)
+print('touristic', count_tc_5)
+print('industrial', count_tc_6)
+print('rural', count_tc_7)
+print('agri-mixed', count_tc_8)
+print('agricultural', count_tc_9)
 
+# total = count_tc_1+count_tc_2+count_tc_8+count_tc_9+count_tc_6+count_tc_7+count_tc_4+count_tc_5+count_tc_3
+# print(total)
 # ------------ STEP 2: data preparation ------------
 # add labels to the modes of transports ID's use the most.
 
