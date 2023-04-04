7ec842f9b476518f7c4f84af6354bd284f14d708
astertournoy <62717135+astertournoy@users.noreply.github.com>
2023-04-04 11:25:27 +0200

    labelled preferred type

--- Subquestion_1.py
+++ Subquestion_1.py
@@ -24,12 +24,24 @@ for idx in df['ID'].unique():
     count_0 = (sub_df['Choice'] == 0).sum()
     count_1 = (sub_df['Choice'] == 1).sum()
     count_2 = (sub_df['Choice'] == 2).sum()
+    # which mode is preferred?
+    if max(count_0,count_1,count_2) == count_0:
+        preferred_mode = 0
+    elif max(count_0,count_1,count_2) ==  count_1:
+        preferred_mode = 1
+    elif max(count_0,count_1,count_2) == count_2:
+        preferred_mode = 2
+    if (count_0 == count_1 and count_0!= 0) or (count_2 == count_1 and count_2 != 0) or (count_2 == count_0 and count_0 != 0):
+        preferred_mode = -2
+    if count_0 == count_1 and count_2 == count_1:
+        preferred_mode = -1
     # Get the value of TC for the current index value
     tc_value = sub_df['TypeCommune'].iloc[0]
     # Add the counts to the dictionary
-    result_dict[idx] = {'public_transport': count_0, 'private_mode': count_1, 'soft_mode': count_2, 'TC': tc_value}
+    result_dict[idx] = {'public_transport': count_0, 'private_mode': count_1, 'soft_mode': count_2, \
+                        'PM': preferred_mode, 'TC': tc_value}
 
-# print(result_dict)
+print(result_dict)
 # print(len(result_dict))
 
 # Create an empty list to store the public_transport values
@@ -64,6 +76,7 @@ plt.show()
 
 
 # Now we want to check how many people live in the different types of communes to have a rough idea.
+# Similarly we want to check how many people prefer what modes
 # Initialize a counter variable to 0
 count_tc_1 = 0
 count_tc_2 = 0
@@ -74,6 +87,11 @@ count_tc_6 = 0
 count_tc_7 = 0
 count_tc_8 = 0
 count_tc_9 = 0
+count_PM_0 = 0
+count_PM_1 = 0
+count_PM_2 = 0
+count_PM_none = 0
+count_PM_multiple = 0
 
 # Loop through each key-value pair in the dictionary
 for idx, values in result_dict.items():
@@ -106,25 +124,42 @@ for idx, values in result_dict.items():
         # If so, increment the counter variable
         count_tc_9 += 1
 
-print('centers', count_tc_1)
-print('suburban', count_tc_2)
-print('high-income', count_tc_3)
-print('periurban', count_tc_4)
-print('touristic', count_tc_5)
-print('industrial', count_tc_6)
-print('rural', count_tc_7)
-print('agri-mixed', count_tc_8)
-print('agricultural', count_tc_9)
+for idx, values in result_dict.items():
+    if values['PM'] == 0:
+        count_PM_0 += 1
+    elif values['PM'] == 1:
+        count_PM_1 += 1
+    elif values['PM'] == 2:
+        count_PM_2 += 1
+    elif values['PM'] == -1:
+        count_PM_none += 1
+    elif values['PM'] == -2:
+        count_PM_multiple += 1
 
-# total = count_tc_1+count_tc_2+count_tc_8+count_tc_9+count_tc_6+count_tc_7+count_tc_4+count_tc_5+count_tc_3
+
+# print('centers', count_tc_1)
+# print('suburban', count_tc_2)
+# print('high-income', count_tc_3)
+# print('periurban', count_tc_4)
+# print('touristic', count_tc_5)
+# print('industrial', count_tc_6)
+# print('rural', count_tc_7)
+# print('agri-mixed', count_tc_8)
+# print('agricultural', count_tc_9)
+# print('public transport', count_PM_0)
+# print('private transport', count_PM_1)
+# print('soft mode', count_PM_2)
+# print('none preferred', count_PM_none)
+# print('multiple preferred', count_PM_multiple)
+
+# total_c = count_tc_1+count_tc_2+count_tc_8+count_tc_9+count_tc_6+count_tc_7+count_tc_4+count_tc_5+count_tc_3
 # print(total)
+# total_pm = count_PM_none + count_PM_multiple + count_PM_0 + count_PM_1 + count_PM_2
+# total_useful = count_PM_0 + count_PM_1 + count_PM_2
+# print(total_useful)
+
+
 # ------------ STEP 2: data preparation ------------
-# add labels to the modes of transports ID's use the most.
-
-
-
-
-
 # split training data and testing data
 
 
