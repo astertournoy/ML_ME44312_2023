f07761221fa5c9f7a357fb79a8ae52300f95f41b
gpans <125491092+gpans@users.noreply.github.com>
2023-04-04 12:03:51 +0200

    Dataset import

--- Subquestion_4.py
+++ Subquestion_4.py
@@ -1,5 +1,9 @@
-import import_data
-df = import_data.df
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+from sklearn.metrics import classification_report
+import Subquestion_1
+import pandas as pd
 
+df = Subquestion_1.result_dict
+df_4 = pd.DataFrame.from_dict(df, orient='index')
 
-# test comment
